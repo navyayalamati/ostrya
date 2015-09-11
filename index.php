12db23e4c8d91e5d46819d56485fc4bc63ab1eef
navyayalamati <navya@ostryalabs.com>
2015-09-11 15:27:31 +0530

    sfgsfd

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -302,7 +302,7 @@ p a:hover { color: red; }
             
             
          
-            <a href="home.html" class="tagc1">Rails</a>
+            <a href="home.php" class="tagc1">Rails</a>
             
             <a href="home.html" class="tagc2">JavaScript</a>
             
